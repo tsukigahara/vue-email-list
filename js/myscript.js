f7b6de336510bd8f62b7da99c4cb6dd7395119ec
tsukigahara <satsuki.tksm@gmail.com>
2022-12-06 18:21:00 +0100

    done

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            list: [],
         }
     },
     methods: {
-        
+        emailGen(){
+            for (let i = 0 ; i < 10; i++){
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    console.log(response.data);
+                    this.list.push(response.data);
+                    console.log(this.list);
+                })
+            }
+        }
+    },
+    beforeMount() {
+        this.emailGen();
     },
 })
 app.mount("#app");
